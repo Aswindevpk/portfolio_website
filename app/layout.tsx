df472041b7600ff62be4fa16d311f0ed8eae6041
aswindevpk <aswindevpk014@gmail.com>
2026-01-09 00:20:10 +0530

    add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -81,6 +82,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer links={[]} />
+        <SpeedInsights />
       </body>
     </html>
   );
